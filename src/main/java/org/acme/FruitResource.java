6e8d0357813f4d3675fc7677288b670e9e4f7145
Raffaele Spazzoli <raffaele.spazzoli@gmail.com>
2022-11-29 14:19:02 -0500

    maybe ready to test

--- src/main/java/org/acme/FruitResource.java
+++ src/main/java/org/acme/FruitResource.java
@@ -7,7 +7,10 @@ import io.smallrye.mutiny.unchecked.Unchecked;
 import io.vertx.mutiny.core.Vertx;
 
 import javax.inject.Inject;
+<<<<<<< HEAD
 import javax.transaction.NotSupportedException;
+=======
+>>>>>>> ab0c502 (maybe ready to test)
 import javax.transaction.SystemException;
 import javax.transaction.TransactionManager;
 import javax.ws.rs.*;
@@ -25,6 +28,7 @@ import java.util.function.Function;
 @Consumes(MediaType.APPLICATION_JSON)
 public class FruitResource {
 
+<<<<<<< HEAD
     long blockTime = 500;
 
     @Inject
@@ -86,5 +90,61 @@ public class FruitResource {
     private Uni<String> undoTask2() {
         return Uni.createFrom().item("undone task2");
     }
+=======
+  long blockTime = 500;
+
+  @Inject
+  Vertx vertx;
+
+  @Inject
+  FruitRepository fruitRepository;
+
+  @Inject TransactionManager tm;
+
+  @Inject
+  Mutiny.Session session;
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public Uni<String> hello() {
+    return Uni.createFrom().item("Hello from RESTEasy Reactive");
+  }
+
+  @POST
+  public Uni<Fruit> create(Fruit fruit,boolean failTask2,boolean failMainTask) throws SystemException {
+    Uni<Fruit> uni=task1(fruit,failTask2,failMainTask);
+    int status=tm.getStatus();
+    if (javax.transaction.Status.STATUS_COMMITTED==status) {
+      return uni;
+    } else {
+      return uni.call(x->undoTask2());
+    }
+  }
+
+  @ReactiveTransactional
+  Uni<Fruit> task1(Fruit fruit,boolean failTask2,boolean failMainTask) throws IllegalStateException, SystemException {
+    if (failMainTask){
+      tm.getTransaction().setRollbackOnly();
+    }
+    return fruitRepository.persist(fruit).
+            // this should roll back when there is a failure
+            onItem().call(x->task2(failTask2));
+  }
+
+  private Uni<String> task2(boolean fail) {
+    return Uni.createFrom().emitter(e -> vertx.setTimer(blockTime, x -> e.complete("completing...")))
+        .map(Unchecked.function(x -> {
+          if (fail)
+            throw new Exception("non-transactional task failed");
+          else {
+            return x.toString();
+          }
+        }));
+  }
+
+  private Uni<String> undoTask2() {
+    return Uni.createFrom().item("undone task2");
+  }
+>>>>>>> ab0c502 (maybe ready to test)
 
 }
